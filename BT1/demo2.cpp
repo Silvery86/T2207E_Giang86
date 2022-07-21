4214d48ff3968703b40e16097add7c966b1bc060
Silvery86 <giang8692@gmail.com>
2022-07-21 20:59:08 +0700

    demo buoi 2

---
+++ BT1/demo2.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(){
+	int a;
+	printf("Nhap a=");
+	scanf("%d",&a);
+	int b;
+	printf("Nhap b=");
+	scanf("%d",&b);
+	if(a>b){
+		printf("Max : %d",a);
+	}else{
+		if(a<b){
+			printf("Max : %d",);
+		}else{
+			printf("Hai so bang ");
+		}
+	}
+}
