1f925542419319bea85cf7598dc30b48f1bf15c2
Silvery86 <giang8692@gmail.com>
2022-07-26 07:18:04 +0700

    Sua lai bai tap 2

--- BTVN2/Bai2.cpp
+++ BTVN2/Bai2.cpp
@@ -13,6 +13,23 @@ int main(){
 	printf("Vui long nhap c=");
 	scanf("%d",&c);
 
+	if(a==0){
+		if(b==0){
+			if(c==0){
+				printf("Phuong trinh co vo so nghiem");
+			}else{
+				printf("Phuong trinh vo nghiem"); 
+			}
+		}else{
+			float x;
+			// Ep kieu du lieu de cho phep tinh dung (kieu du lieu)
+			// Co the ep kieu du lieu len to hon hoac xuong nho hon
+			// float pi = 3.14159;
+			// int p2 = (int)pi;
+			x= -(float)c/b;
+			printf("Phuong trinh co nghiem la: %f",x);
+		}
+	}else{
 	float d;
 
 	d = ((pow(b,2)) - (4*a*c));
@@ -24,17 +41,17 @@ int main(){
 		x1 = (-b + sqrt(d))/(2*a);
 		x2 = (-b - sqrt(d))/(2*a);
 		printf( "Phuong trinh co 2 nghiem la %f va %f",x1,x2);
-		
+
 	}else{
 		if(
 			d<0){
 			printf("Phuong trinh vo nghiem");
-		
-			
+
 		}else{
-			float x3;
-			x3 = ( -b / (2*a) );
-			printf("Phuong trinh co nghien kep la %f",x3);
+			float x;
+			x = ( -(float)b / (2*a) );
+			printf("Phuong trinh co nghien kep la %f",x);
 		}
-}
+		}
+	}
 }
