73cb8ef57950af3467d856007a2f1f00ceb06a12
Silvery86 <giang8692@gmail.com>
2022-08-01 20:06:03 +0700

    BT

---
+++ BT6/Array.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main()
+{
+	int n;
+	printf("Nhap n=");
+	scanf("%d",&n);
+	int ary[n];
+	int high;
+	int x;
+	
+	printf("Nhap 10 so nguyen:\n");
+
+	for(int i=0;i<n;i++){
+		scanf("%d",&ary[i]);
+	}
+	high = ary[0];
+	x = ary[0];
+
+	for(int i=1;i<n;i++){
+		if(ary[i] > high){
+			high = ary[i];
+			}
+		if(ary[i]>x && ary[i] != high){
+			x = ary[i];
+		}
+
+		}
+		printf("\n Gia tri lon nhat la: %d",high);
+		printf("\n Gia tri gan lon thu 2 la: %d",x);
+}
