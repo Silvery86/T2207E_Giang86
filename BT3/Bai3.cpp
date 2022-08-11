b649f08df99e36f70acb22f5f036d5a1e4c97373
Silvery86 <giang8692@gmail.com>
2022-08-11 20:32:48 +0700

    Bo sung

--- BT3/Bai3.cpp
+++ BT3/Bai3.cpp
@@ -3,11 +3,14 @@ int main(){
 	int n;
 	printf("Vui long nhap n=");
 	scanf("%d",&n);
-
-	float S=0;
-	for(int i=1;i<=n;i++){
-		S = (i-1) + (i-2);
+	int arr[n];
+	int F;
+	arr[0]=1;
+	arr[1]=1;
+	for(int i=2;i<n;i++){
+		arr[i] = arr[i-1] + arr[i-2];
+		F = arr[i];
 		}
-	printf("S= %f",S);
+	printf("So fibonaci thu n = %d",F);
 
 }
