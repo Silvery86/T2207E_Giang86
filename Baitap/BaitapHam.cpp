2c1fdf269087d9336b343f2ef20cfcab780de282
Silvery86 <giang8692@gmail.com>
2022-08-11 21:10:30 +0700

    sua

--- Baitap/BaitapHam.cpp
+++ Baitap/BaitapHam.cpp
@@ -1,13 +1,19 @@
 #include <stdio.h>
 // Viet ham kiem tra 1 so co phai la so nguyen to 
 bool ktSoNguyenTo(int n){
+	if(n>1){
 	for(int i=2;i<n;i++){
 		if(n%i==0){
 			return false;
 		}
 	}
 	return true;
+}else{
+	return false;
 }
+}
+
+
 
 int main(){
 	int n;
